fd9aadef1c2608932c36f5227253492429de2482
Bang210 <bangvelope@gmail.com>
2023-12-07 11:44:29 +0900

    회원 컨트롤러 추가

---
+++ src/main/java/com/ll/medium/domain/member/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.ll.medium.domain.member.controller;
+
+
+import com.ll.medium.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/signup")
+    public String join() {
+        return "signup_form";
+    }
+}
