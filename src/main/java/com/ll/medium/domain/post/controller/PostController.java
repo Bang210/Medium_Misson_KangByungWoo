eef875a3d948e496dd0d3098bc05db4d022a95f5
Bang210 <bangvelope@gmail.com>
2023-12-09 15:47:34 +0900

    write, detail, links

--- src/main/java/com/ll/medium/domain/post/controller/PostController.java
+++ src/main/java/com/ll/medium/domain/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.ll.medium.domain.post.controller;
 
 import com.ll.medium.domain.member.entity.Member;
 import com.ll.medium.domain.member.service.MemberService;
+import com.ll.medium.domain.post.entity.Post;
 import com.ll.medium.domain.post.form.WriteForm;
 import com.ll.medium.domain.post.service.PostService;
 import com.ll.medium.global.rq.Rq;
@@ -9,11 +10,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -38,4 +42,19 @@ public class PostController {
         postService.create(writeForm.getTitle(), writeForm.getBody(), member);
         return rq.redirect("/", "등록되었습니다.");
     }
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        List<Post> postList = postService.findAll();
+        model.addAttribute("postList", postList);
+        return "/post/post_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String showDetail(Model model, @PathVariable("id") Long id) {
+        Post post = postService.getPostById(id);
+        model.addAttribute("post", post);
+        return "/post/post_detail";
+    }
+
 }
--- src/main/java/com/ll/medium/domain/post/service/PostService.java
+++ src/main/java/com/ll/medium/domain/post/service/PostService.java
@@ -3,10 +3,13 @@ package com.ll.medium.domain.post.service;
 import com.ll.medium.domain.member.entity.Member;
 import com.ll.medium.domain.post.entity.Post;
 import com.ll.medium.domain.post.repository.PostRepository;
+import com.ll.medium.global.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +23,19 @@ public class PostService {
         post.setAuthor(member);
         post.setCreateDate(LocalDateTime.now());
         postRepository.save(post);
+    }
+
+    public List<Post> findAll() {
+        List<Post> postList = postRepository.findAll();
+        return postList;
+    }
+
+    public Post getPostById(Long id) {
+        Optional<Post> opPost = postRepository.findById(id);
+        if (opPost.isPresent()) {
+            return opPost.get();
+        } else {
+            throw new DataNotFoundException("post not found");
+        }
     }
 }
